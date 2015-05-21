c0df83d7e00e3171151c7d66db2a8cf156d23c02
Sys <sys@netvibes.com>
2015-05-21 10:47:37 +0200

    Fix routes for Redmine3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-match '/gif/:filename.gif', :to => 'gif_uploader#download'
-match '/gif/upload', :to => 'gif_uploader#upload'
-match '/gif/gallery', :to => 'gif_uploader#gallery'
+match '/gif/:filename.gif', :to => 'gif_uploader#download', :via => :get
+match '/gif/upload', :to => 'gif_uploader#upload', :via => :post
+match '/gif/gallery', :to => 'gif_uploader#gallery', :via => :get
